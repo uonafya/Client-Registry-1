a44d158cb6fd3b7c5fada07e3bf78edc76595330
Ian-Njuguna <njugunaayan@gmail.com>
2022-03-03 09:17:00 +0300

    basic auth

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,7 +8,7 @@ class LoginController extends Controller
 {
     public function login(Request $reqs)
     {
-        dd(request('email'), request('password'));
+        // dd(request('email'), request('password'));
         
         $this->validate($reqs, [
             'email' => 'required|email',
